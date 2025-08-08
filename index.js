1333cec35717ab1944ece130d483764b8b453fc1
root8Op <130396783+root8Op@users.noreply.github.com>
2025-08-08 12:28:35 +0530

    changes maded on a surface the avtar had changed, the js  file has been updated, got proper alignment

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-gsap.registerPlugin(ScrollTrigger);
-const sections = gsap.utils.toArray(".section");
-gsap.to(sections,{
-    
 
-  xPercent: -100 * (sections.length - 1),
-  scrollTrigger: {
-    trigger: "#mainFrame",
-    toggleActions: "play none none reverse",
-    start: "top top",
-    pin: true,
-    scrub: 1,
-    snap: 1 / (sections.length - 1),
-    end: () => "+=" + document.querySelector("#mainFrame").offsetWidth,
-    invalidateOnRefresh: true,
-  }
-});
